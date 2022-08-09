31bba9e6242daebb8abd4bf265aaecaa87106014
Oksanka25 <chekoksa25@gmail.com>
2022-08-09 16:16:32 -0400

    Ex3-4 done

--- Day2/JS_Excercises/operators.js
+++
@@ -1,4 +0,0 @@
-let num1 = 3;
-let num2 = 5;
-let sum = num1 + num2;
-console.log(sum);
---
+++ operators.js
@@ -0,0 +1,36 @@
+//Exercise Find the sum
+
+let num = 3;
+let anotherNum = 5;
+let sum = num + anotherNum;
+console.log(sum);
+
+let numX = 5;
+let numY = 6;
+let numB = 7;
+let sum1 = numB + numY + numX;
+console.log(sum1);
+
+// Exercise Find the Average
+
+num1 = 4;
+num2 = 10;
+num3 = 22;
+num4 = -30;
+num5 = 55;
+
+avg = (num1 + num2 + num3 + num4 + num5) / 5;
+console.log(avg);
+
+num6 = 68;
+num7 = 7;
+num8 = -22;
+num9 = 9;
+num10 = 100;
+
+average = (num6 + num7 + num8 + num9 + num10) / 5;
+console.log(average);
+
+allAverage = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10) / 10;
+
+console.log(allAverage);
