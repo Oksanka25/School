f173c004dcef12b29b8778657764f6832d1396c1
Oksanka25 <chekoksa25@gmail.com>
2022-08-10 09:05:07 -0400

    a little fixing

--- Day2/JS_Excercises/operators.js
+++ Day2/JS_Excercises/operators.js
@@ -38,6 +38,6 @@ num10 = 100;
 average = (num6 + num7 + num8 + num9 + num10) / 5;
 console.log(average);
 
-allAverage = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10) / 10;
+allAverage = (avg + average) / 2;
 
 console.log(allAverage);
