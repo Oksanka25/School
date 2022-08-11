c12b8c2c11374ac5c01be9a8dc498c30b3cca1b3
Oksanka25 <chekoksa25@gmail.com>
2022-08-11 19:37:00 -0400

    sumArray function is done

--- Day4/hw_functions.js
+++ Day4/hw_functions.js
@@ -26,7 +26,7 @@ const maxOfThreeNumbers = (a, b, c) => {
 // console.log(maxOfThreeNumbers(7, 13, 0));
 
 // 3. Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
-const isCharAVowel = (char) => {
+function isCharAVowel(char) {
     let vowels = ['a', 'e', 'y', 'i', 'u', 'o'];
     for (let i = 0; i < vowels.length; i++) {
         if (char.toLowerCase() === vowels[i]) {
@@ -35,19 +35,33 @@ const isCharAVowel = (char) => {
     }
     return false
 }
-console.log(isCharAVowel('a'))
-console.log(isCharAVowel('b'))
-console.log(isCharAVowel('Y'))
+// console.log(isCharAVowel('a'))
+// console.log(isCharAVowel('b'))
+// console.log(isCharAVowel('Y'))
 
 
-// 4. Define a function, as a function expression, sumArraythat takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
+// 4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let j = 0; j < arr.length; j++) {
+        sum += arr[j];
+    }
+    return sum
+}
 
-// 5. Define a function, as a function declaration, multiplyArraythat takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+// console.log(sumArray([2, 4, 5]))
 
-// 6. Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
 
-// 7. Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
 
-// 8. Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string.
+// 5. Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
 
-// 9. Define a function, as a function declaration, stringsLongerThanthat takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
+
+// 6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
+
+
+
+// 7. Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+
+// 8. Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
+
+// 9. Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
