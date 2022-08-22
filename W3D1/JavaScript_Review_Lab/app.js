76d2468b25b6c8bd5fc6b86210f5f94a6bac5076
Oksanka25 <chekoksa25@gmail.com>
2022-08-22 12:45:23 -0400

    -Section C is done

--- W3D1/JavaScript_Review_Lab/app.js
+++ W3D1/JavaScript_Review_Lab/app.js
@@ -31,3 +31,21 @@ console.log(`Hello, my name is ${myName}`);
 
 
 // C. Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' == 'Name');
+// FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log(false || false || false || false || false || true);
+console.log(false == false)
+console.log(e === 'Kevin');
+console.log(a + b == c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 == '48');
