608a1043bfc009b8d7336766f8e6c797d7c6f14d
Oksanka25 <chekoksa25@gmail.com>
2022-08-26 14:05:50 -0400

    The conditions are set for each player

--- W3D5/tic_tac_toe_game/app.js
+++ W3D5/tic_tac_toe_game/app.js
@@ -17,4 +17,53 @@ function changePlayer() {
     else if (player === "O") player = "X";
 }
 
+squares.forEach(square => {
+    square.addEventListener("click", () => {
+        if (square.innerHTML === "" && activeGame === true) {
+            square.innerHTML = player;
+            changePlayer();
+            moves++;
+            if (moves === 9) illegal.innerHTML = "It's a tie! Reset a game and start over!"
+        } else if (square.innerHTML) {
+            illegal.innerHTML = "Don't cheat!"
+        } else {
+            illegal.innerHTML = "Someone has already won! Reset the game!"
+        }
+        checkResult();
+    });
+})
+
+const checkResult = () => {
+    const arrSq = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [0, 4, 8]
+    ];
+
+    if (activeGame === true) {
+        for (i = 0; i < arrSq.length; i++) {
+            if (squares[arrSq[i][0]].innerHTML == "X" && squares[arrSq[i][1]].innerHTML == "X" && squares[arrSq[i][2]].innerHTML == "X") {
+                result = "Player X wins!";
+                activeGame = false;
+                scoreX();
+                prepareResult(result);
+                break;
+
+            } else if (squares[arrSq[i][0]].innerHTML == "O" && squares[arrSq[i][1]].innerHTML == "O" && squares[arrSq[i][2]].innerHTML == "O") {
+                result = "Player O wins!";
+                scoreO();
+                prepareResult(result);
+                activeGame = false;
+                break;
+            }
+        }
+    }
+}
+
+
 
