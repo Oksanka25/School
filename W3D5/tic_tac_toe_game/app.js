bd588b05c6a8bc11db46d317457a402134e5b6a5
Oksanka25 <chekoksa25@gmail.com>
2022-08-26 13:45:49 -0400

    Started working on logic of the game

---
+++ W3D5/tic_tac_toe_game/app.js
@@ -0,0 +1,13 @@
+let squares = document.querySelectorAll('.square')
+console.log(squares);
+let player = "X";
+let illegal = document.querySelector(".illegal");
+let result = "";
+let startGame = document.querySelector(".start");
+let resetGame = document.querySelector(".reset");
+let activeGame = true;
+let moves = 0;
+let xCount = document.querySelector(".xwins")
+let oCount = document.querySelector(".owins")
+let xWins = 0;
+let oWins = 0;
