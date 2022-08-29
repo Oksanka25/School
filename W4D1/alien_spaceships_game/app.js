02275460941844a64f2c8b6b8411d61cc827b503
Oksanka25 <chekoksa25@gmail.com>
2022-08-29 14:48:17 -0400

    Created and array of alien ships

--- W4D1/alien_spaceships_game/app.js
+++ W4D1/alien_spaceships_game/app.js
@@ -6,3 +6,12 @@ function startGame() {
         return false
     }
 }
+
+const alienShips = [
+    { name: 'War Fighter 1', hull: 3, firepower: 2, accuracy: .6 },
+    { name: 'War Fighter 2', hull: 4, firepower: 2, accuracy: .6 },
+    { name: 'War Fighter 4', hull: 5, firepower: 3, accuracy: .7 },
+    { name: 'War Fighter 5', hull: 6, firepower: 3, accuracy: .7 },
+    { name: 'Alien Lieutenant', hull: 5, firepower: 3, accuracy: .7 },
+    { name: 'Alien General', hull: 6, firepower: 4, accuracy: .8 }
+];
