b2a50746df586c7340a37c29f0ebe03757c2c32a
Oksanka25 <chekoksa25@gmail.com>
2022-08-30 13:11:55 -0400

    Ship class with basic functionality is created

--- W4D1/alien_spaceships_game/app.js
+++ W4D1/alien_spaceships_game/app.js
@@ -8,93 +8,61 @@ let enemyStats = document.querySelector('enemy-stats');
 let gameBoard = document.querySelector('.gameboard');
 
 
-
-
-
-function startGame() {
-    let welcome = prompt('Welcome to Space Battle, Earth is under attack and in need of assistance! Will you Aid in the survival of Earth? Y/N');
-    if (welcome.toLowerCase() == 'y') {
-        return true
-    } else {
-        return false
-    }
+// Function for random integer between 'min' and 'max'
+function randomBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-
-class Spaceship {
-    constructor() {
-        this.name = 'USS HelloWorld'
-        this.hull = 20
-        this.firepower = 5
-        this.accuracy = 0.7
+class Ship {
+    constructor(name) {
+        this.name = name;
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = 0.7;
     }
-};
-
-console.log(Spaceship)
-
-const alienShip = [
-    { name: 'War Fighter 1', hull: 3, firepower: 2, accuracy: .6 },
-    { name: 'War Fighter 2', hull: 4, firepower: 2, accuracy: .6 },
-    { name: 'Alien Lieutenant', hull: 5, firepower: 3, accuracy: .7 },
-    { name: 'War Fighter 4', hull: 5, firepower: 3, accuracy: .7 },
-    { name: 'War Fighter 5', hull: 6, firepower: 3, accuracy: .7 },
-    { name: 'Alien General', hull: 6, firepower: 4, accuracy: .8 }
-];
-console.log(alienShip)
-
-// startGame()
-
-function attackShip() {
-    if (startGame() === true) {
-        alert('Awesome! Lets get started!')
-        for (let i = 0; i < alienShip.length; i++) {
-            // console.log(alienShip[i].hull)
-            let progress = prompt('Attack!!! You have them scared now! Attack / Retreat');
-            if (progress.toLowerCase() === 'a') {
-                alert('They showed up to the WRONG PLANET')
-                battle(alienShip[i])
+    attack(enemy) {
+        // Use of accuracy to determine a hit
+        if (Math.random() < this.accuracy) {
+            gameBoard.innerHTML = `${this.name} attacked ${enemy.name} with ${this.firepower} firepower!`
+            console.log(`${this.name} attacked ${enemy.name} with ${this.firepower} firepower!`);
+            // If attacking Mega-Ship, decrease the Enemy ship's hull equal to this ship's firepower
+            if (enemy.name === 'Alien Mega-Ship') {
+                enemy.isAttackedBy(this);
             } else {
-                alert('Hopefully the next commander will finish the job')
-                return
-            }
-        } if (Spaceship.hull > 0) {
-            console.log('You have saved Earth!')
-            return Spaceship.hull
-        } else {
-            console.log('The Commander has fallen!')
-            return
-        }
-    } else {
-        alert('Well Earth needed someone more reliable anyways')
-
-        return
-    }
-}
-
-
-function battle(alienShip) {
-    while (alienShip.hull > 0 && Spaceship.hull > 0) {
-        // console.log('hello')
-        // console.log(Spaceship.accuracy)       
-        if (Spaceship.accuracy >= Math.random()) {
-            // console.log('hello')
-            alienShip.hull -= Spaceship.firepower // attackShip(alienShipOne, myShip)    
-            console.log(alienShip)
-            console.log('DIRECT HIT!!!')
-            if (alienShip.hull <= 0) {
-                console.log('The ship went kabloo-ey')
-            } else if (alienShip.accuracy >= Math.random()) {
-                Spaceship.hull -= alienShip.firepower
-                console.log(`Your hull integrity is at ${Spaceship.hull}`)
-            } else if (Spaceship.hull <= 0) {
-                console.log('The commander has been lost!')
-            } else {
-                console.log('They are still alive!')
+                // If attacking normally
+                enemy.hull -= this.firepower;
             }
         } else {
-            console.log('Miss!!!')
+            gameBoard.innerHTML = `${this.name}'s shots missed ${enemy.name}!`
+            // console.log(`${this.name}'s shots missed ${enemy.name}!`);
         }
     }
+    target(group, enemyName) {
+        // Find the specific Enemy alien ship in the array of ships
+        let index = group.findIndex(ship => ship.name === enemyName);
+
+        // If Target Index = -1, means Ship with ship.name is not in the group array anymore
+        if (index === -1) {
+            gameBoard.innerHTML = `${enemyName} is not a valid selection.`
+            // console.log(`${enemyName} is not a valid selection.`);
+            return false;
+        } else {
+            // Attack the targeted ship
+            this.attack(group[index]);
+            // If Enemy ship is destroyed, remove from array of ships
+            if (group[index].hull <= 0) {
+                gameBoard.innerHTML = `${group[index].name} was destroyed!`
+                // console.log(`${group[index].name} was destroyed!`);
+                group.splice(index, 1);
+            }
+            return true;
+        }
+    }
+    shield() {
+        // Random value of shield will be added to hull
+        let shieldVal = parseInt(randomBetween(1, 10));
+        gameBoard.innerHTML = `${this.name} shielded itself by ${shieldVal}.`;
+        console.log(`${this.name} shielded itself by ${shieldVal}.`);
+        this.hull += shieldVal;
+    }
 }
-setTimeout(attackShip, 500)
-// attackShip()
