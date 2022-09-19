2f36a650918753de68a963962a99278f8ea2d22a
Oksanka25 <chekoksa25@gmail.com>
2022-09-19 10:21:33 -0400

    Create new route is working

--- W7D1/fruits_express_app/server.js
+++ W7D1/fruits_express_app/server.js
@@ -5,20 +5,46 @@ const app = express();
 const fruits = require("./models/fruits")
 
 // Middleware
+// 5. It runs in the middle of the request response cycle
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
+app.use(express.urlencoded({ extended: false }));
 
-//Views
+//Views/ configurations here
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 // Routes
-// Index - GET
+// 1. Index - GET
 app.get('/fruits', (req, res) => {
     res.render('Index', { allFruits: fruits });
     // res.send(fruits); // this was before the react view
 });
 
-//add show route
+// 3.put this above your Show route
+// new
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+
+// 4. Create a post route
+app.post('/fruits', (req, res) => {
+    if (req.body.readyToEat === 'on') { //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    // res.send('data received'); // this first to check if the route is working
+    res.redirect('/fruits'); //send the user back to /fruits
+});
+
+
+// 2. add show route
 // Show - GET
 app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     res.render('Show', { //second param must be an object
--- W7D1/fruits_express_app/views/Index.jsx
+++ W7D1/fruits_express_app/views/Index.jsx
@@ -5,6 +5,9 @@ class Index extends React.Component {
         const { allFruits } = this.props;
         return (
             <div>
+                <nav>
+                    <a href="/fruits/new">Create a New Fruit</a>
+                </nav>
                 <h1>Fruits Index Page</h1>
                 <ul>
                     {allFruits.map((fruit, i) => {
