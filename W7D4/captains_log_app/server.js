96169dab38ce2cf934097d2c058897c1612ba7f0
Oksanka25 <chekoksa25@gmail.com>
2022-09-22 19:19:28 -0400

    Connected to MongoDB

--- W7D4/captains_log_app/server.js
+++ W7D4/captains_log_app/server.js
@@ -3,7 +3,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// 2. Requiring and Initializing
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+require("dotenv").config()
 
+
+// 2.1 Middleware
+// defines the view engine
+app.set("view engine", "jsx");
+// Creates link to JSX
+app.engine("jsx", require("express-react-views").createEngine());
+// instantiates MethodOverride for CRUD actions
+app.use(methodOverride("_method"))
+// to Parse req.body
+app.use(express.urlencoded({ extended: false }))
+// establishes Middleware Process
+app.use((req, res, next) => {
+    console.log('I am here for there routes ')
+    next()
+})
+
+mongoose.connect(process.env.MONGO_URI
+);
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+});
 
 
 
