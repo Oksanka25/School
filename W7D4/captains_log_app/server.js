7693873d7ee61bfff9709547d32597d12668648b
Oksanka25 <chekoksa25@gmail.com>
2022-09-22 19:10:19 -0400

    Initialized the app, port is working

---
+++ W7D4/captains_log_app/server.js
@@ -0,0 +1,19 @@
+// 1. Boilerplate
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1. Boilerplate
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
