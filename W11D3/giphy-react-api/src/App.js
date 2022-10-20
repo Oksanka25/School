99f0a8126a48dff2bc14d2e670b9386043725a1b
Oksanka25 <chekoksa25@gmail.com>
2022-10-20 10:09:10 -0400

    Create components

--- W11D3/giphy-react-api/src/App.js
+++ W11D3/giphy-react-api/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import SearchContainer from './components/SearchContainer';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className='flex container justify-content-center'>
+      <h1 className='d-flex justify-content-center mb-3 top-heading'> Search for Giphy</h1>
+
+      <SearchContainer />
+
     </div>
   );
 }
